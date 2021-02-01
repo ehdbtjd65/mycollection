64dba183f1cd3a23a05c0cbce337b78b29991fc1
ehdbt <ehdbt@DESKTOP-0PGRSN2>
2021-02-01 17:41:49 +0900

    pdf -> png 변환 기능 완성

--- src/main/java/com/spring/mycollection/util/PdfToimges.java
+++ src/main/java/com/spring/mycollection/util/PdfToimges.java
@@ -1,20 +1,52 @@
 package com.spring.mycollection.util;
 
+import java.awt.image.BufferedImage;
 import java.io.File;
-
+import java.io.IOException;
 
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.rendering.ImageType;
+import org.apache.pdfbox.rendering.PDFRenderer;
+import org.apache.pdfbox.tools.imageio.ImageIOUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class PdfToimges {
-	@SuppressWarnings("unchecked")
-	public static void main(String args) throws Exception {
 
-		PDDocument document = new PDDocument();
-		PDPage newPage = new PDPage();
-		document.addPage(newPage);
-		document.save(new File("C:/Users/test.pdf"));
-		document.close();
+	private static final Logger logger = LoggerFactory.getLogger(PdfToimges.class);
 
+
+
+
+
+
+	public void saveUploadedFile(String pdfFilename) {
+
+		int pageCounter = 0;
+		logger.info("PdfToimges 클래스에 들어왔다");
+		
+		PDDocument document;
+		try {
+			document = PDDocument.load(new File(pdfFilename));
+
+			PDFRenderer pdfRenderer = new PDFRenderer(document);
+
+			for (PDPage page : document.getPages()) {
+				// note that the page number parameter is zero based
+				BufferedImage bim = pdfRenderer.renderImageWithDPI(pageCounter, 300, ImageType.RGB);
+
+				logger.info("이미지수" + page + "  /   name " + pdfFilename);
+				// suffix in filename will be used as the file format
+				ImageIOUtil.writeImage(bim, pdfFilename + "-" + (pageCounter++) + ".png", 300);
+			}
+			logger.info("총 이미지수" + pageCounter);
+			document.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	
 	}
 }
